efb3a0770a4a245ccd8c47c94528df6400e43a54
Zavizhenetc <zavizhenetc@gmail.com>
2020-10-30 00:12:17 +0300

    деплой, корректировки в разметке

--- src/js/constants/constants.js
+++ src/js/constants/constants.js
@@ -2,31 +2,31 @@ export { RESULT_ITEMS, CARDS}
 
 const RESULT_ITEMS = [
   {
-    linkImg: 'src/images/404_page.png',
+    linkImg: './images/404_page.png',
   title: 'Название категории',
   query: '134 товара',
   
 },
   {
-    linkImg: 'src/images/404_page.png',
+    linkImg: './images/404_page.png',
   title: 'Название категории',
   query: '134 товара',
   
 },
   {
-  linkImg: 'src/images/404_page.png',
+    linkImg: './images/404_page.png',
   title: 'Название категории',
   query: '134 товара',
   
 },
   {
-    linkImg: 'src/images/404_page.png',
+    linkImg: './images/404_page.png',
   title: 'Название категории',
   query: '134 товара',
   
 },
   {
-    linkImg: 'src/images/404_page.png',
+    linkImg: './images/404_page.png',
   title: 'Название категории',
   query: '134 товара',
   
@@ -35,37 +35,37 @@ const RESULT_ITEMS = [
 ]
 const CARDS = [
   {
-    linkImg: 'src/images/404_page.png',
+    linkImg: './images/404_page.png',
   title: 'Наименование товара, которое не более 2 строк',
   price: 'XX XXX  $',
   
 },
   {
-    linkImg: 'src/images/404_page.png',
+    linkImg: './images/404_page.png',
   title: 'Наименование товара, которое не более 2 строк',
   price: 'XX XXX  $',
   
 },
   {
-    linkImg: 'src/images/404_page.png',
+    linkImg: './images/404_page.png',
   title: 'Наименование товара, которое не более 2 строк',
   price: 'XX XXX  $',
   
 },
   {
-    linkImg: 'src/images/404_page.png',
+    linkImg: './images/404_page.png',
   title: 'Наименование товара, которое не более 2 строк',
   price: 'XX XXX  $',
   
 },
   {
-    linkImg: 'src/images/404_page.png',
+    linkImg: './images/404_page.png',
   title: 'Наименование товара, которое не более 2 строк',
   price: 'XX XXX  $',
   
 },
   {
-    linkImg: 'src/images/404_page.png',
+    linkImg: './images/404_page.png',
   title: 'Наименование товара, которое не более 2 строк',
   price: 'XX XXX  $',
   
